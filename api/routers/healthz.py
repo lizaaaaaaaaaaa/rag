a895452b2b6bef8ea9426c52415f3a18ee216511
Roi <roi@example.com>
2025-05-21 21:39:23 +0900

    test trigger

--- api/routers/healthz.py
+++ api/routers/healthz.py
@@ -11,7 +11,7 @@ def healthz():
     try:
         conn = psycopg2.connect(
             host=os.getenv("DB_HOST"),
-            port=os.getenv("DB_PORT", 5432),
+            port=int(os.getenv("DB_PORT", 5432)),
             database=os.getenv("DB_NAME"),
             user=os.getenv("DB_USER"),
             password=os.getenv("DB_PASSWORD"),
